518f816d6bb6600e51a1cde99bd13ba20ee42b2e
Anthony COUDENE <anthony.coudene@technidata-web.com>
2024-10-17 14:59:52 +0200

    Add a starting point for ssh client/server

---
+++ SSHClientApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using Renci.SshNet;
+
+using (var client = new SshClient("localhost", 2222, "anthony", "P@ssw0rd"))
+{
+  client.Connect();
+  using ShellStream shellStream = client.CreateShellStream("ShellName", 80, 24, 800, 600, 1024);
+  client.Disconnect();
+}
