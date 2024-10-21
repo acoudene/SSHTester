523a647da1e932ac9b52ee57daefc8d4054ab4fa
Anthony COUDENE <anthony.coudene@technidata-web.com>
2024-10-21 16:01:08 +0200

    SSH Exceed MaxStartup simulator

--- SSHClientApp/Program.cs
+++ SSHClientApp/Program.cs
@@ -1,8 +1,24 @@
 ﻿using Renci.SshNet;
 
-using (var client = new SshClient("localhost", 2222, "anthony", "P@ssw0rd"))
+Parallel.For(0, 100, async i =>
 {
-  client.Connect();
-  using ShellStream shellStream = client.CreateShellStream("ShellName", 80, 24, 800, 600, 1024);
-  client.Disconnect();
-}
+  using (var client = new SshClient("localhost", 2222, "anthony", "P@ssw0rd"))
+  {
+    client.Connect();
+    // Make the server echo back the input file with "cat"
+    using (SshCommand command = client.CreateCommand("cat"))
+    {
+      Task executeTask = command.ExecuteAsync(CancellationToken.None);
+
+      using (Stream inputStream = command.CreateInputStream())
+      {
+        inputStream.Write("Hello World!"u8);
+      }
+
+      await executeTask;
+
+      Console.WriteLine(command.ExitStatus); // 0
+      Console.WriteLine(command.Result); // "Hello World!"
+    }
+  }
+});
