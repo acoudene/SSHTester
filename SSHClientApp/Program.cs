9920a01ba836ff1a4c4134466106a3bcd5242112
Anthony COUDENE <anthony.coudene@technidata-web.com>
2024-10-21 16:10:02 +0200

    Add Exception management to continue and illustrate hazardous results

--- SSHClientApp/Program.cs
+++ SSHClientApp/Program.cs
@@ -2,23 +2,31 @@
 
 Parallel.For(0, 100, async i =>
 {
-  using (var client = new SshClient("localhost", 2222, "anthony", "P@ssw0rd"))
+  try
   {
-    client.Connect();
-    // Make the server echo back the input file with "cat"
-    using (SshCommand command = client.CreateCommand("cat"))
+    using (var client = new SshClient("localhost", 2222, "anthony", "P@ssw0rd"))
     {
-      Task executeTask = command.ExecuteAsync(CancellationToken.None);
-
-      using (Stream inputStream = command.CreateInputStream())
+      client.Connect();
+      // Make the server echo back the input file with "cat"
+      using (SshCommand command = client.CreateCommand("cat"))
       {
-        inputStream.Write("Hello World!"u8);
+        Task executeTask = command.ExecuteAsync(CancellationToken.None);
+
+        using (Stream inputStream = command.CreateInputStream())
+        {
+          inputStream.Write("Hello World!"u8);
+        }
+
+        await executeTask;
+
+        Console.WriteLine(command.ExitStatus); // 0
+        Console.WriteLine(command.Result); // "Hello World!"
       }
-
-      await executeTask;
-
-      Console.WriteLine(command.ExitStatus); // 0
-      Console.WriteLine(command.Result); // "Hello World!"
     }
   }
+  catch (Exception ex)
+  {
+    Console.WriteLine(ex.Message);
+  }
+
 });
